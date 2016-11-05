3fd654c180167a7d62eba2f28439511090e1f0d5
Szymon Grotha <szymon.grotha@gmail.com>
2016-11-05 15:22:22 +0100

    First Version of JS Triangle Area

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+{
+	var a = prompt('Podaj długość podstawy');
+	var h = prompt('Podaj długość wysokości');
+	var triangleArea = a * h /2;
+
+	alert('Pole Tójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' +triangleArea);
+	console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+}
